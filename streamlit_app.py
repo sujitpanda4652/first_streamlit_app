40ee8f0f97e7a4ff4ec0f79f0941431f49035157
sujit panda <117503451+sujitpanda4652@users.noreply.github.com>
2023-09-17 15:31:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit
 
 streamlit.title('My Diet Plan')
 streamlit.header('Breakfast Menu')
-streamlit.text('Beetroot juice')
+streamlit.text('🥣 Beetroot juice')
 streamlit.header('Lunch')
-streamlit.text('Brown Rice & Veg curry')
+streamlit.text('🐔Brown Rice & chicken')
 streamlit.header('Dinner')
-streamlit.text('Roti eggs')
+streamlit.text('🥗Roti eggs')
 
 streamlit.header('Hey Anu How are you?')
 streamlit.text('Are you ok!!')
